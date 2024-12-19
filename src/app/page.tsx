ed5ea99ed9b7985ca8f5fdfe73de5c370b2cc0dd
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-12-19 08:48:47 +0300

    feat: setup one-tap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-export function getServerStaticParams() {}
+"use client";
+import GoogleOneTap from "@/components/GoogleOneTap";
 
-export default async function Home() {
+// TODO:
+// connect t supabase for storing notes
+// make Oauth to authorize people who leave notes + setup NextAuth
+// make home screen ui showing all notes
+// add ui for authentication and creating a note
+// promote MVP and leave links to my github
+
+export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
+      {/* <button onClick={handleClick}>Auth</button> */}
+      <GoogleOneTap />
       <p>Say hello</p>
     </div>
   );
